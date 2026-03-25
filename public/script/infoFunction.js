b5538d8305198b7cab77cf6c2014d18b02c10b19
Sam J <sam_joseph@live.com>
2026-03-25 11:43:33 -0700

    fix(overlay): disable hover on touch devices to allow mobile tap

--- public/script/infoFunction.js
+++ public/script/infoFunction.js
@@ -2,16 +2,25 @@ function overlay(){
     const info = document.querySelector(".info");
     const helpOverlay = document.querySelector(".help-overlay");
 
+    const istouch = 'ontouchstart' in window;
+
     // Prevent errors on pages that don't have these elements for future page
     if (!info || !helpOverlay) return;
 
-
-    info.addEventListener("mouseenter", () => {
+    // Desktop hover
+    if(!istouch){
+        info.addEventListener("mouseenter", () => {
+            helpOverlay.classList.add("visible");
+        });
+    };
+    
+    // Mobile + desktop click
+   info.addEventListener("click", () => {
+        console.log("INFO CLICKED");
         helpOverlay.classList.add("visible");
     });
 
-   
-
+    // Close overlay on click
     helpOverlay.addEventListener("click", () => {
         helpOverlay.classList.remove("visible");
     });
